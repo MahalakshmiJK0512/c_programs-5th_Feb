b80e8a77382dbcc906589dda715fadb11d9e16a7
MahalakshmiJK0512 <163166565+MahalakshmiJK0512@users.noreply.github.com>
2025-02-06 06:12:50 +0530

    Print the name

---
+++ Print the name.c
@@ -0,0 +1,19 @@
+//Printing the name//
+#include <stdio.h>
+    int main()
+    {
+        char a;
+        printf("My name is: ");
+        scanf("%c",&a);
+return 0;
+    }
+//Get the name from user//
+#include <stdio.h>
+
+int main() {
+    char name[50];
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+return 0;
+}
